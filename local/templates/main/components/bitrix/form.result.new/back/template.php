5f047391b0791dfcf01059d5e28626455e5f83c5
Unavailable-f <85505263+Unavailable-f@users.noreply.github.com>
2021-09-29 16:45:20 +0700

    Update template.php

--- local/templates/main/components/bitrix/form.result.new/back/template.php
+++ local/templates/main/components/bitrix/form.result.new/back/template.php
@@ -3,8 +3,10 @@ if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
 
 if ($arResult["isFormErrors"] == "Y"):?><?=$arResult["FORM_ERRORS_TEXT"];?><?endif;?>
 
+
 <?if ($arResult["isFormNote"] != "Y")
 {
+
 ?>
 
 <div class="contact-form">
@@ -17,75 +19,53 @@ if ($arResult["isFormErrors"] == "Y"):?><?=$arResult["FORM_ERRORS_TEXT"];?><?end
 
 	<?=str_replace('<form', '<form class="contact-form__form"', $arResult["FORM_HEADER"]) ?> 
 
-
-
-    <?foreach ($arResult["QUESTIONS"] as $FIELD_SID => $arQuestion): ?>
-    <?if ($arQuestion['STRUCTURE'][0]['ID'] == 1) {
-    $name = "form_text_1";
-    $id = "medicine_name";
-    $type = "text";
-}   elseif ($arQuestion['STRUCTURE'][0]['ID'] == 2) {
-    $name = "form_text_2";
-    $id = "medicine_company";
-    $type = "text";
-}   elseif ($arQuestion['STRUCTURE'][0]['ID'] == 3) {
-    $name = "form_email_3";
-    $id = "medicine_email";
-    $type = "email";
-}   elseif ($arQuestion['STRUCTURE'][0]['ID'] == 4) {
-    $name = "form_text_4";
-    $id = "medicine_phone";
-    $type = "tel";
-}   elseif ($arQuestion['STRUCTURE'][0]['ID'] == 5) {
-    $name = "form_textarea_5";
-    $id = "medicine_message";
-    $type = "text";
-}
-
-?>
-<? if ($arQuestion['STRUCTURE'][0]['ID'] == 1):?>
     <div class="contact-form__form-inputs">
-<?endif;?>    
-
-<? if ($arQuestion['STRUCTURE'][0]['ID'] < 5):?>
-<div class="input contact-form__input"><label class="input__label" for=<?= $id?>>
-                <div class="input__label-text"><?= $arQuestion["CAPTION"]?></div>
-                <input class="input__input" type=<?=$type?> id=<?= $id?> name=<?= $name?> value=""
+            <div class="input contact-form__input"><label class="input__label" for="medicine_name">
+                <div class="input__label-text"><?= $arResult["QUESTIONS"]["SIMPLE_QUESTION_930"]["CAPTION"]?></div>
+                <input class="input__input" type="text" id="medicine_name" name="form_text_1" value=""
                        required="">
- 
+                <div class="input__notification">Поле должно содержать не менее 3-х символов</div>
             </label></div>
-<?endif;?>             
-
-<? if ($arQuestion['STRUCTURE'][0]['ID'] == 4):?>
-</div>
-
-<div class="contact-form__form-message">
+            <div class="input contact-form__input"><label class="input__label" for="medicine_company">
+                <div class="input__label-text"><?= $arResult["QUESTIONS"]["SIMPLE_QUESTION_950"]["CAPTION"]?></div>
+                <input class="input__input" type="text" id="medicine_company" name="form_text_2" value=""
+                       required="">
+                <div class="input__notification">Поле должно содержать не менее 3-х символов</div>
+            </label></div>
+            <div class="input contact-form__input"><label class="input__label" for="medicine_email">
+                <div class="input__label-text"><?= $arResult["QUESTIONS"]["SIMPLE_QUESTION_983"]["CAPTION"]?></div>
+                <input class="input__input" type="email" id="medicine_email" name="form_email_3" value=""
+                       required="">
+                <div class="input__notification">Неверный формат почты</div>
+            </label></div>
+            <div class="input contact-form__input"><label class="input__label" for="medicine_phone">
+                <div class="input__label-text"><?= $arResult["QUESTIONS"]["SIMPLE_QUESTION_701"]["CAPTION"]?></div>
+                <input class="input__input" type="tel" id="medicine_phone"
+                       data-inputmask="'mask': '+79999999999', 'clearIncomplete': 'true'" maxlength="12"
+                       x-autocompletetype="phone-full" name="form_text_4" value="" required=""></label></div>
+        </div>
+        <div class="contact-form__form-message">
             <div class="input"><label class="input__label" for="medicine_message">
-                <div class="input__label-text">Сообщение</div>
+                <div class="input__label-text"><?= $arResult["QUESTIONS"]["SIMPLE_QUESTION_522"]["CAPTION"]?></div>
                 <textarea class="input__input" type="text" id="medicine_message" name="form_textarea_5"
                           value=""></textarea>
+                <div class="input__notification"></div>
             </label></div>
-</div>
-
-<?endif;?> 
-
-<?endforeach;?>
 
 
-
+            
+        </div>
         <div class="contact-form__bottom">
             <div class="contact-form__bottom-policy">Нажимая &laquo;Отправить&raquo;, Вы&nbsp;подтверждаете, что
                 ознакомлены, полностью согласны и&nbsp;принимаете условия &laquo;Согласия на&nbsp;обработку персональных
                 данных&raquo;.
             </div>
-			<input class="form-button contact-form__bottom-button" <?=(intval($arResult["F_RIGHT"]) < 10 ? "disabled=\"disabled\"" : "");?> type="submit" name="web_form_submit" value="Оставить заявку"/>
+            <input class="form-button contact-form__bottom-button" <?=(intval($arResult["F_RIGHT"]) < 10 ? "disabled=\"disabled\"" : "");?> type="submit" name="web_form_submit" value="Оставить заявку"/>
         </div>
+        
+ 
 		<?=$arResult["FORM_FOOTER"]?>
 </div>
 
-
 <?
 } //endif (isFormNote)
-
-
-
